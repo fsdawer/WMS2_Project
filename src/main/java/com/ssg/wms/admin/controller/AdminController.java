1e916d2d35792bceb3eac29afc0883a923caa428
dynii <dyniiyeyo@naver.com>
2025-11-10 02:19:44 +0900

    config 추가: WebConfig, RoleCheckInterceptor

--- src/main/java/com/ssg/wms/admin/controller/AdminController.java
+++ src/main/java/com/ssg/wms/admin/controller/AdminController.java
@@ -1,11 +1,16 @@
 package com.ssg.wms.admin.controller;
 
+import com.ssg.wms.admin.dto.MemberSearchCriteriaDTO;
+import com.ssg.wms.admin.dto.StaffDTO;
 import com.ssg.wms.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
@@ -16,12 +21,31 @@ public class AdminController {
 
     @GetMapping("/")
     public String getAdminMain() {
+        // 메인 화면
         return "admin/connect";
     }
 
     @GetMapping("/login")
     public String getAdminLogin() {
+        // 로그인 화면
         return "admin/login";
     }
 
+    @GetMapping("/mypage")
+    public String getUserInfo(HttpSession session, Model model) {
+        // 마이페이지 조회
+        return "admin/mypage";
+    }
+
+    @GetMapping("/members")
+    public String getMembers(@ModelAttribute MemberSearchCriteriaDTO criteria, HttpSession session) {
+        // 고객 목록 조회
+        return "admin/members";
+    }
+
+    @GetMapping("/memebers/{memberId}")
+    public String getMembersDetail(@PathVariable long memberId, HttpSession session) {
+        // 고객 상세 조회
+        return "admin/members/{memberId}";
+    }
 }
